bae3ee78bca467da77b3332056d675dc9560aa6d
skidy89 <tutorialeyg310@gmail.com>
2024-08-16 00:04:27 -0600

    idk

--- src/Utils/auth-utils.ts
+++ src/Utils/auth-utils.ts
@@ -33,7 +33,7 @@ export function makeCacheableSignalKeyStore(
 			const data: { [_: string]: SignalDataTypeMap[typeof type] } =  { }
 			const idsToFetch: string[] = []
 			for(const id of ids) {
-				const item = await cache!.get<SignalDataTypeMap[typeof type]>(getUniqueId(type, id))
+				const item = await cache.get<SignalDataTypeMap[typeof type]>(getUniqueId(type, id))
 				if(typeof item !== 'undefined') {
 					data[id] = item
 				} else {
@@ -125,7 +125,7 @@ export const addTransactionCapability = (
 					}, { }
 				)
 			} else {
-				return state.get(type, ids)
+				return await state.get(type, ids)
 			}
 		},
 		set: data => {
--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -258,8 +258,25 @@ export const prepareWAMessageMedia = async(
 	}
 
 	if(cacheableKey) {
+		// set cache
 		logger?.debug({ cacheableKey }, 'setting cache')
-		await options.mediaCache!.set(cacheableKey, WAProto.Message.encode(obj).finish())
+		const encImg = WAProto.Message.fromObject({
+			[`${mediaType}Message`]: MessageTypeProto[mediaType].fromObject(
+				{
+					url: mediaUrl,
+					directPath,
+					mediaKey,
+					fileEncSha256,
+					fileSha256,
+					fileLength,
+					mediaKeyTimestamp: unixTimestampSeconds(),
+					...uploadData,
+					media: undefined
+				}
+			)
+		})
+		await options.mediaCache!.set(cacheableKey, WAProto.Message.encode(encImg).finish())
+
 	}
 
 	return obj
